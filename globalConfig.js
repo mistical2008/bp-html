8292c9b3167f33677a574ca77e79e5522919d025
Evgeniy Talko <etalko@gmail.com>
2021-08-18 10:41:55 +0300

    Add(config): font-magician config

--- globalConfig.js
+++ globalConfig.js
@@ -1,13 +1,13 @@
 // https://github.com/csstools/postcss-font-magician
 // for using in css without @font-face rule
 exports.fonts = {
-  // variants: {
-  // "Open Sans": {
-  // 300: [],
-  // 400: [],
-  // 700: [],
-  // },
-  // },
-  // foundries: ["google"],
+  variants: {
+    "Open Sans": {
+      300: [],
+      400: [],
+      700: [],
+    },
+  },
+  foundries: ["google"],
   hosted: ["./src/assets/fonts"],
 };
