78ce6cc788e33bacf51f5219a0f865e9915b88f0
Evgeniy Talko <etalko@gmail.com>
2021-08-26 16:45:10 +0300

    Add(conf): postcss config for prettier

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   fix: true,
-  extends: "stylelint-config-rational-order",
+  extends: [
+   "stylelint-config-rational-order",
+   "stylelint-config-prettier",
+  ],
   plugins: ["stylelint-declaration-strict-value"],
   rules: {
     "scale-unlimited/declaration-strict-value": [
