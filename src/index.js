b02f3d387ded92809192531604b446fe995ea6c6
Evgeniy Talko <etalko@gmail.com>
2021-10-27 10:50:28 +0300

    Chore(js): move main entry code. Fix paths.
    * Move all runtime code from 'index.js' to './js/main.js'
    * Fix paths in './js/main.js'

--- src/index.js
+++ src/index.js
@@ -1,30 +1,6 @@
+import { devLogEntryLoaded } from "./js/utils/debug";
 import './css/index.css';
-import {toggleActive} from './js/handlers';
-import {fixViewportHeight} from './js/utils';
+import './js/main'
 
-console.log('Main index.js entry file');
+devLogEntryLoaded('Main entry file')
 
-const navSelector = 'menu__nav-wrapper';
-const navSelectorActive = 'menu__nav-wrapper_active';
-const navBtnSelector = 'js-nav-menu-icon';
-
-function toggleNavMenu(navSelector, navSelectorActive) {
-  toggleActive(navSelector, navSelectorActive);
-}
-
-// Main runtime function
-function main() {
-  fixViewportHeight();
-
-  // >>> Place your code there
-  document.addEventListener('click', evt => {
-    if (evt.target === document.querySelector(`.${navBtnSelector}`)) {
-      toggleNavMenu(navSelector, navSelectorActive);
-    }
-  });
-  // <<< Place your code there
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  main();
-});
