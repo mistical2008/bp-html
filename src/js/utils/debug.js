13efdf52addab4d1bce04a88416f96dfb28c293b
Evgeniy Talko <etalko@gmail.com>
2021-10-27 10:48:00 +0300

    Add(utils): current file loading status in dev mode

--- src/js/utils/debug.js
+++ src/js/utils/debug.js
@@ -4,4 +4,14 @@ function printFormData(formData) {
   }
 }
 
-export {printFormData};
+/**
+* @description logs current file loaded while NODE_ENV === 'development'
+* @param {string} optional file purpose description. logged to console.
+*/
+function devLogEntryLoaded(filePurpose = 'Entry') {
+  if (process.env.NODE_ENV === "development") {
+    console.log(`${filePurpose} loaded: `, __filename);
+  }
+}
+
+export {printFormData, devLogEntryLoaded};
